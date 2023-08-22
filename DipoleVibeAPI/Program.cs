ae43dff8f1fad48f5e6998b5e1df64c00c0f08f5
Tobechukwu David Uka <ukadavid7@gmail.com>
2023-08-22 20:19:40 +0100

    Project Setup Done

--- DipoleVibeAPI/Program.cs
+++ DipoleVibeAPI/Program.cs
@@ -1,4 +1,5 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Infrastructure;
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +7,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
 
